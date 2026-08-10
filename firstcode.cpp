8b16c7baef11b9d8dc3f8624eb41c36848daf7c2
santosh018 <skbhandariii07@gmail.com>
2026-08-10 11:50:07 +0530

    Add files via upload

---
+++ firstcode.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int age = 25;
+    cout <<"size of int : " <<sizeof(int) <<endl;
+
+    char ch = 'A';
+    cout <<"size of char : " <<sizeof(ch) <<endl;
+
+    float pi =3.14f;
+    cout <<"size of float : " <<sizeof(float) <<endl;
+
+
+    double pre =3.141595;
+    cout <<"size of double : " <<sizeof(double) <<endl;
+
+    bool iscoding = true;
+    cout <<"size of bool : " <<sizeof(bool) <<endl;
+
+    return 0;
+}
